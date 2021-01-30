3505929b20baa1ce7a7b6546eccf3d3da7cec00d
Mayuri Chaudhari <mayurichaudhari57gmail.com>
2021-01-30 15:50:44 +0530

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { :host => 'new_app_name.herokuapp.com' }  
+  config.action_mailer.default_url_options = { :host => 'gentle-inlet-00322.herokuapp.com' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
@@ -118,8 +118,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            ENV["GMAIL_EMAIL"],
-  password:             ENV["GMAIL_PASSWORD"],
+  user_name:            'mayurichaudhari57@gmail.com',
+  password:             'ottyrqawzunqlpmw',
   authentication:       'plain',
   enable_starttls_auto: true  }
 end
