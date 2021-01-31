ac35e7c03b274b1476f2150815f77b722f68fa9d
Mayuri Chaudhari <mayurichaudhari57gmail.com>
2021-01-31 13:34:25 +0530

    asset resource

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
-//= require_tree .
-window.liveSettings={api_key:"d9697d9b0f384c699944b913131835b6"}
+//= require_tree 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
